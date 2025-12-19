f4ef01030b9527b5838f23a63e377df3e9cc9f39
MatteoMel1985 <108396022+MatteoMel1985@users.noreply.github.com>
2025-12-19 20:02:28 +0800

    Add files via upload

---
+++ ETL_toll_data.py
@@ -0,0 +1,77 @@
+# import the libraries
+from datetime import timedelta
+# The DAG object; we'll need this to instantiate a DAG
+from airflow.models import DAG
+# Operators; you need this to write tasks!
+from airflow.operators.bash_operator import BashOperator
+# This makes scheduling easy
+from airflow.utils.dates import days_ago
+#defining DAG arguments
+
+default_args = {
+    'owner': 'Artifex Datorum',
+    'start_date': days_ago(0),
+    'email': ['memirloni@gmail.com'],
+    'email_on_failure': True,
+    'email_on_retry': True,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+# defining the DAG
+
+# define the DAG
+dag = DAG(
+    'ETL_toll_data',
+    default_args=default_args,
+    description='Apache Airflow Final Assignment',
+    schedule_interval=timedelta(days=1),
+)
+
+# define the tasks
+# define the first task (unzipping data)
+unzip_data = BashOperator(
+    task_id='unzip_data',
+    bash_command='tar -xzvf /home/project/airflow/dags/finalassignment/tolldata.tgz -C /home/project/airflow/dags/finalassignment',
+    dag=dag,
+)
+
+# define the second task (extracting from csv)
+extract_data_from_csv = BashOperator(
+    task_id= 'extract_data_from_csv',
+    bash_command= 'cut -d"," -f1-4 < /home/project/airflow/dags/finalassignment/vehicle-data.csv > /home/project/airflow/dags/finalassignment/csv_data.csv',
+    dag= dag,
+)
+
+# define the third task (extracting from tsv)
+extract_data_from_tsv = BashOperator(
+    task_id='extract_data_from_tsv',
+    bash_command='cut -f5-7 < /home/project/airflow/dags/finalassignment/tollplaza-data.tsv > /home/project/airflow/dags/finalassignment/tsv_data.csv',
+    dag=dag,
+)
+
+# define the fourth task (extracting from txt)
+extract_data_from_fixed_width = BashOperator(
+    task_id='extract_data_from_fixed_width',
+    bash_command='cut -c60-62,64-68 < /home/project/airflow/dags/finalassignment/payment-data.txt > /home/project/airflow/dags/finalassignment/fixed_width_data.csv',
+    dag=dag,
+)
+
+# define the fifth task (consolidating the data)
+consolidate_data = BashOperator(
+    task_id= 'consolidate_data',
+    bash_command= 'paste -d, /home/project/airflow/dags/finalassignment/csv_data.csv /home/project/airflow/dags/finalassignment/tsv_data.csv'
+    '/home/project/airflow/dags/finalassignment/fixed_width_data.csv > /home/project/airflow/dags/finalassignment/extracted_data.csv',
+    dag= dag,
+)
+
+# define the sixth task (transform)
+transform_data = BashOperator(
+    task_id='transform_data',
+    bash_command='tr "[a-z]" "[A-Z]" < /home/project/airflow/dags/finalassignment/extracted_data.csv > /home/project/airflow/dags/finalassignment/staging/transformed_data.csv',
+    dag=dag,
+)
+
+unzip_data >> extract_data_from_csv >> extract_data_from_tsv >> extract_data_from_fixed_width >> consolidate_data >> transform_data
+
+
